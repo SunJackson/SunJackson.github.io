c8051a1b8ca6a9d506be515c259b340b069bcadf
SunJackson <jiajia.sun@yunfangdata.com>
2018-07-23 10:09:30 +0800

    2018-07-23

--- js/jquery.js
+++ js/jquery.js
@@ -5778,7 +5778,7 @@ function augmentWidthOrHeight( elem, name, extra, isBorderBox, styles ) {
 		val = 0;
 
 	for ( ; i < 4; i += 2 ) {
-		// Both box models exclude margin, so add it if we want it
+		// Both box Models exclude margin, so add it if we want it
 		if ( extra === "margin" ) {
 			val += jQuery.css( elem, extra + cssExpand[ i ], true, styles );
 		}
